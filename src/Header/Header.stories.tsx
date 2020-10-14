a78980f23368403c2bf991663e0d5f4705c1fdf8
fullsnackdeveloper <thefullsnackdeveloper@gmail.com>
2020-10-13 22:22:26 -0400

    update header

--- src/Header/Header.stories.tsx
+++ src/Header/Header.stories.tsx
@@ -6,7 +6,7 @@ export default {
     component: Header
 };
 
-export const Fullwidth = () => <Header
+export const Default = () => <Header
     title="Header Title"
     description="We’re here to help you out with whatever project you are working on, let’s DIY together!"
     image="https://images.unsplash.com/photo-1541123437800-1bb1317badc2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -6,12 +6,11 @@ import { HeaderProps } from "./Header.types";
 import clsx from "clsx";
 
 const Header: FC<HeaderProps> = ({ title, description, image }: HeaderProps) => (
-    <div className={clsx("Header")}>
+    <div className={clsx("Header")} style={{ backgroundImage: `linear-gradient(90deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0) 60%), url(${image})` }}>
         <div className={clsx("Header-content")}>
             <h1>{title}</h1>
             <p>{description}</p>
         </div>
-        <div className="Header-image" style={{ backgroundImage: `url(${image})` }}></div>
     </div>
 );
 
