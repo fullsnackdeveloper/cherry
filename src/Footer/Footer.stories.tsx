089b65d238810b2dfa247e48c7a145998be60369
fullsnackdeveloper <thefullsnackdeveloper@gmail.com>
2020-11-27 14:28:17 -0500

    footer fix

--- src/Footer/Footer.stories.tsx
+++ src/Footer/Footer.stories.tsx
@@ -3,6 +3,7 @@ import Footer from "./Footer";
 import Link from '../Link/Link';
 import React from "react";
 import Row from '../Row/Row';
+import footerBG from '../images/footer-bg.png';
 
 export default {
     title: "Footer",
