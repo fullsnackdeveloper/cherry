9d712f95a9521d3fbdf198d18fd085996d7fc7d1
fullsnackdeveloper <thefullsnackdeveloper@gmail.com>
2020-12-09 19:09:17 -0500

    add Block capabilities

--- src/Block/Block.tsx
+++ src/Block/Block.tsx
@@ -4,8 +4,8 @@ import { BlockProps } from "./Block.types";
 import React from "react";
 import clsx from "clsx";
 
-const Block: React.FC<BlockProps> = ({ html, children, isHeading }) => (
-    <div data-testid="Block" className={clsx("Block", { isHeading })} dangerouslySetInnerHTML={html && { __html: html }}>{children}</div>
+const Block: React.FC<BlockProps> = ({ html, children, isHeading, pullDown, pullUp }) => (
+    <div data-testid="Block" className={clsx("Block", { isHeading, pullDown, pullUp })} dangerouslySetInnerHTML={html && { __html: html }}>{children}</div>
 );
 
 export default Block;
--- src/Block/Block.types.ts
+++ src/Block/Block.types.ts
@@ -4,4 +4,6 @@ export interface BlockProps {
     children?: ReactNode;
     html?: string;
     isHeading?: boolean;
+    pullUp?: boolean;
+    pullDown?: boolean;
 }
