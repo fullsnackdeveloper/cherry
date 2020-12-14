98a0793321139c5191d23658609d5be57ddab11a
fullsnackdeveloper <thefullsnackdeveloper@gmail.com>
2020-12-14 13:38:46 -0500

    desgin update

--- src/Recommendation/Recommendation.tsx
+++ src/Recommendation/Recommendation.tsx
@@ -4,12 +4,14 @@ import Icon from "../Icon/Icon";
 import Link from "../Link/Link";
 import React from "react";
 import { RecommendationProps } from "./Recommendation.types";
+import _ from 'lodash';
 
 const Recommendation: React.FC<RecommendationProps> = ({ type, title, description, image, uri }) => {
     return <div data-testid="Recommendation" className="Recommendation">
         <div className="Recommendation-text">
+            <h3>Recommended</h3>
             <h2>{title}</h2>
-            <p>{description}</p>
+            <p>{_.truncate(description, { length: 120 })}</p>
             <Link>Read more<Icon icon="chevron-right" size={10} /></Link>
         </div>
         <div className="Recommendation-image" style={{ backgroundImage: `url(${image?.src})` }}>
